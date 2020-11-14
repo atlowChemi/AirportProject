898148f8b558c02d0d1f5bf77467b3a1d0d0e5fd
Chemi Atlow <chemiatlow@gmail.com>
2020-11-14 19:12:43 +0200

    Remove controllers

---
+++ Server/Hubs/FlightHub.cs
@@ -0,0 +1,27 @@
+﻿using Common.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Hubs
+{
+    public class FlightHub : Hub
+    {
+        public ICollection<Airplane> GetAirplanes()
+        {
+            return new Airplane[]{ new Airplane(), new Airplane(), new Airplane() };
+        }
+
+        public Flight CreateFlight(Flight flight)
+        {
+            return flight;
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+    }
+}
