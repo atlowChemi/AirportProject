e0075fb48a75138c432ce4eb8fdbe18181bead39
Chemi Atlow <chemiatlow@gmail.com>
2020-11-15 11:02:35 +0200

    Add models

---
+++ Common/Models/Flight.cs
@@ -0,0 +1,19 @@
+﻿using Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Common.Models
+{
+    public class Flight
+    {
+        public Guid Id { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public FlightDirection Direction { get; set; }
+        public virtual int AirplaneId { get; set; }
+        public virtual Airplane Airplane { get; set; }
+        public virtual ICollection<FlightHistory> History { get; set; }
+    }
+}
---
+++ Common/Models/FlightHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Models
+{
+    public class FlightHistory
+    {
+        public Guid Id { get; set; }
+        public DateTime? EnterStationTime { get; set; }
+        public DateTime? LeaveStationTime { get; set; }
+        public virtual Guid FlightId { get; set; }
+        public virtual Flight Flight { get; set; }
+        public virtual Guid StationId { get; set; }
+        public virtual Station Station { get; set; }
+    }
+}
