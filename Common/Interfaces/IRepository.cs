8c84014cfa16a80d9a19eeecd17d3d28e1c1f06f
Chemi Atlow <chemiatlow@gmail.com>
2020-11-16 09:00:34 +0200

    Create generic repository

---
+++ Common/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class, new()
+    {
+        IQueryable<TEntity> GetAll();
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+    }
+}
---
+++ DAL/Repositories/Repository.cs
@@ -0,0 +1,78 @@
+﻿using Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        private readonly AirportContext AirportContext;
+
+        public Repository(AirportContext airportContext)
+        {
+            AirportContext = airportContext;
+        }
+
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            try
+            {
+                return AirportContext.Set<TEntity>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Couldn't retrieve entities: {ex.Message}");
+            }
+        }
+
+        public virtual async Task<TEntity> AddAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            }
+            try
+            {
+                EntityEntry<TEntity> entry = await AirportContext.AddAsync(entity);
+                await AirportContext.SaveChangesAsync();
+                return entry.Entity;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Issue while saving to DB: {dbEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be saved: {ex.Message}");
+            }
+        }
+
+        public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            }
+            try
+            {
+                AirportContext.Update(entity);
+                await AirportContext.SaveChangesAsync();
+                return entity;
+            }
+
+            catch (DbUpdateException dbEx)
+            {
+                throw new Exception($"Issue while saving to DB: {dbEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{nameof(entity)} could not be updated: {ex.Message}");
+            }
+        }
+    } 
+}
