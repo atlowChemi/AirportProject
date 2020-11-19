3638fe7cc3b8cf6703640b8fbfa975f55e377cf1
Chemi Atlow <chemiatlow@gmail.com>
2020-11-19 08:37:59 +0200

    Create a random data generator

---
+++ BL/Services/RandomDataGeneratorService.cs
@@ -0,0 +1,16 @@
+﻿using Common.Interfaces;
+using System;
+
+namespace BL.Services
+{
+    public class RandomDataGeneratorService : IRandomDataGeneratorService
+    {
+        private readonly Random random = new Random(DateTime.UtcNow.Millisecond);
+
+        public int CreateRandomNumber(int min, int max)
+        {
+            if (max < min) throw new ArgumentOutOfRangeException(nameof(max), "Maximum range can't be smaller than minimum range!");
+            return random.Next(min, max);
+        }
+    }
+}
---
+++ Common/Interfaces/IRandomDataGeneratorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Interfaces
+{
+    public interface IRandomDataGeneratorService
+    {
+        int CreateRandomNumber(int min = 0, int max = int.MaxValue);
+    }
+}
