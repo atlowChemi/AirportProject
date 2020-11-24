aa633d96a9381f80f5a7202c184df0903f79ec3e
Chemi Atlow <chemiatlow@gmail.com>
2020-11-24 19:33:23 +0200

    Complete the flight tables component

--- WebClient/app/src/views/FlightTables.vue
+++ WebClient/app/src/views/FlightTables.vue
@@ -1,16 +1,37 @@
 <template>
     <div>
-        <flight-table title="Landing" />
-        <flight-table title="Takeoff" />
+        <flight-table title="Landing" :flights="landingFlights" />
+        <flight-table title="Takeoff" :flights="takeoffFlights" />
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 import FlightTable from '@/components/FlightTable.vue';
+import { FlightDirection } from '@/models/FlightDirection';
+import { data } from '@/services';
 
 const component = defineComponent({
     components: { FlightTable },
+    setup() {
+        const landingFlights = computed(() =>
+            data.flights
+                .filter(f => f.direction == FlightDirection.Landing)
+                .sort(
+                    (a, b) =>
+                        Date.parse(a.plannedTime) - Date.parse(b.plannedTime),
+                ),
+        );
+        const takeoffFlights = computed(() =>
+            data.flights
+                .filter(f => f.direction == FlightDirection.Takeoff)
+                .sort(
+                    (a, b) =>
+                        Date.parse(a.plannedTime) - Date.parse(b.plannedTime),
+                ),
+        );
+        return { landingFlights, takeoffFlights };
+    },
 });
 
 export default component;
