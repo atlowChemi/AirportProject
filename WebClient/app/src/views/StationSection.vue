256124acf2a84f4c28255e363e6f6b505f627ad3
Chemi Atlow <chemiatlow@gmail.com>
2020-11-29 14:53:46 +0200

    Fix mode

--- WebClient/app/src/views/StationSection.vue
+++ WebClient/app/src/views/StationSection.vue
@@ -6,19 +6,14 @@
         full-page
     >
         <template #table-title>
-            <Switch
-                v-model:mode="mode"
-                mode1="list"
-                mode2="flow"
-                @update:mode="modeChanged"
-            />
+            <Switch v-model:mode="mode" mode1="list" mode2="flow" />
         </template>
         <template #default v-if="data.stations.length"><slot /></template>
     </Table>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, ref } from 'vue';
+import { defineComponent, computed } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import { data } from '@/services/AirportService';
 import Switch from '@/components/ToggleSwitch/Switch.vue';
@@ -31,16 +26,18 @@ const component = defineComponent({
         const route = useRoute();
 
         const isTableView = computed(() => route.name === 'ListView');
-        const mode = ref(route.name === 'ListView' ? 1 : 2);
-        const modeChanged = (mode: number) => {
-            const name = mode == 1 ? 'ListView' : 'FlowView';
-            router.push({ name });
-        };
+        const mode = computed({
+            get: () => (route.name === 'ListView' ? 1 : 2),
+            set: mode => {
+                const name = mode == 1 ? 'ListView' : 'FlowView';
+                router.push({ name });
+            },
+        });
         const tableHeaders = {
             columns: '1fr 2fr 4rem',
             data: ['name', 'state', 'more'],
         };
-        return { mode, modeChanged, tableHeaders, isTableView, data };
+        return { mode, tableHeaders, isTableView, data };
     },
 });
 
