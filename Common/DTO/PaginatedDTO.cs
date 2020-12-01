0131ff9f87a0e0a6023222ca14ad4984da20796d
Chemi Atlow <chemiatlow@gmail.com>
2020-12-01 21:55:10 +0200

    Complete DTO documentation

--- Common/DTO/PaginatedDTO.cs
+++ Common/DTO/PaginatedDTO.cs
@@ -2,9 +2,19 @@
 
 namespace Common.DTO
 {
+    /// <summary>
+    /// A pagination wrappwer for any required element.
+    /// </summary>
+    /// <typeparam name="T">Type of paginated data.</typeparam>
     public class PaginatedDTO<T>
     {
+        /// <summary>
+        /// All the elements in current page.
+        /// </summary>
         public IEnumerable<T> Elements { get; init; }
+        /// <summary>
+        /// Total count of elements, including this page.
+        /// </summary>
         public int Total { get; init; }
     }
 }
