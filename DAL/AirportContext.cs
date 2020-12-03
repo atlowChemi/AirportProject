78af81bc163254636903c7a59634177fcb2e79e3
Chemi Atlow <chemiatlow@gmail.com>
2020-12-03 08:00:27 +0200

    Create extension methods for the DbContext usage (#8)
    * Create extension methods for the DbContext usage
    
    * attempt using extension methods

--- DAL/AirportContext.cs
+++ DAL/AirportContext.cs
@@ -1,5 +1,6 @@
 ﻿using Common.Models;
 using DAL.DummyData;
+using DAL.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace DAL
@@ -43,86 +44,11 @@ namespace DAL
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            SetControlTowerNameToBeUnique(modelBuilder);
-            DefineStationFlightRelation(modelBuilder);
-            DefineStationToStationRelation(modelBuilder);
-            DefineStationToControlTowerRelation(modelBuilder);
-            InjectPrePopulatedData(modelBuilder);
-        }
-
-
-        /// <summary>
-        /// Defince the control tower model, and set it's name unique.
-        /// </summary>
-        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-        private void SetControlTowerNameToBeUnique(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ControlTower>()
-                .HasIndex(ct => ct.Name)
-                .IsUnique();
-        }
-        /// <summary>
-        /// Define the relation of station and flight.
-        /// </summary>
-        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-        private void DefineStationFlightRelation(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Station>()
-                .HasOne(s => s.CurrentFlight)
-                .WithOne(f => f.Station)
-                .HasForeignKey<Station>(s => s.CurrentFlightId);
-        }
-        /// <summary>
-        /// Define the control tower ans station relations.
-        /// </summary>
-        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-        private void DefineStationToControlTowerRelation(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<StationControlTowerRelation>(entity =>
-            {
-                entity.HasKey(sctr => new { sctr.StationToId, sctr.Direction, sctr.ControlTowerId });
-                entity
-                    .HasOne(sctr => sctr.Station)
-                    .WithOne(s => s.ControlTowerRelation)
-                    .HasForeignKey<StationControlTowerRelation>(sctr => sctr.StationToId);
-                entity
-                    .HasOne(sctr => sctr.ControlTower)
-                    .WithMany(ct => ct.FirstStations)
-                    .HasForeignKey(sctr => sctr.ControlTowerId);
-            });
-        }
-        /// <summary>
-        /// Define the self relation of the stations.
-        /// </summary>
-        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-        private void DefineStationToStationRelation(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<StationRelation>(entity =>
-            {
-                entity.HasKey(sr => new { sr.StationToId, sr.StationFromId, sr.Direction });
-                entity
-                    .HasOne(sr => sr.StationFrom)
-                    .WithMany(s => s.ChildrenStations)
-                    .HasForeignKey(sr => sr.StationFromId);
-                entity
-                    .HasOne(sr => sr.StationTo)
-                    .WithMany(s => s.ParentStations)
-                    .HasForeignKey(sr => sr.StationToId);
-            });
-        }
-        /// <summary>
-        /// Inject (seed) data into the DB.
-        /// </summary>
-        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-        private void InjectPrePopulatedData(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Airplane>().HasData(PrePopulateData.Airplanes);
-            modelBuilder.Entity<Flight>().HasData(PrePopulateData.Flights);
-            modelBuilder.Entity<Station>().HasData(PrePopulateData.Stations);
-            modelBuilder.Entity<StationRelation>().HasData(PrePopulateData.StationRelations);
-            modelBuilder.Entity<FlightHistory>().HasData(PrePopulateData.FlightHistories);
-            modelBuilder.Entity<ControlTower>().HasData(PrePopulateData.ControlTowers);
-            modelBuilder.Entity<StationControlTowerRelation>().HasData(PrePopulateData.StationControlTowerRelations);
+            modelBuilder.SetControlTowerNameToBeUnique()
+                .DefineStationFlightRelation()
+                .DefineStationToStationRelation()
+                .DefineStationToControlTowerRelation()
+                .InjectPrePopulatedData();
         }
     }
 }
---
+++ DAL/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,93 @@
+﻿using Common.Models;
+using DAL.DummyData;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        /// <summary>
+        /// Defince the control tower model, and set it's name unique.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+        public static ModelBuilder SetControlTowerNameToBeUnique(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ControlTower>()
+                .HasIndex(ct => ct.Name)
+                .IsUnique();
+            return modelBuilder;
+        }
+
+        /// <summary>
+        /// Define the relation of station and flight.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+        public static ModelBuilder DefineStationFlightRelation(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Station>()
+                .HasOne(s => s.CurrentFlight)
+                .WithOne(f => f.Station)
+                .HasForeignKey<Station>(s => s.CurrentFlightId);
+            return modelBuilder;
+        }
+
+        /// <summary>
+        /// Define the control tower ans station relations.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+        public static ModelBuilder DefineStationToControlTowerRelation(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StationControlTowerRelation>(entity =>
+            {
+                entity.HasKey(sctr => new { sctr.StationToId, sctr.Direction, sctr.ControlTowerId });
+                entity
+                    .HasOne(sctr => sctr.Station)
+                    .WithOne(s => s.ControlTowerRelation)
+                    .HasForeignKey<StationControlTowerRelation>(sctr => sctr.StationToId);
+                entity
+                    .HasOne(sctr => sctr.ControlTower)
+                    .WithMany(ct => ct.FirstStations)
+                    .HasForeignKey(sctr => sctr.ControlTowerId);
+            });
+            return modelBuilder;
+        }
+
+        /// <summary>
+        /// Define the self relation of the stations.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+        public static ModelBuilder DefineStationToStationRelation(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StationRelation>(entity =>
+            {
+                entity.HasKey(sr => new { sr.StationToId, sr.StationFromId, sr.Direction });
+                entity
+                    .HasOne(sr => sr.StationFrom)
+                    .WithMany(s => s.ChildrenStations)
+                    .HasForeignKey(sr => sr.StationFromId);
+                entity
+                    .HasOne(sr => sr.StationTo)
+                    .WithMany(s => s.ParentStations)
+                    .HasForeignKey(sr => sr.StationToId);
+            });
+            return modelBuilder;
+        }
+
+        /// <summary>
+        /// Seed to the DB how the intialliztation.
+        /// </summary>
+        /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+        /// <returns></returns>
+        public static ModelBuilder InjectPrePopulatedData(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Airplane>().HasData(PrePopulateData.Airplanes);
+            modelBuilder.Entity<Flight>().HasData(PrePopulateData.Flights);
+            modelBuilder.Entity<Station>().HasData(PrePopulateData.Stations);
+            modelBuilder.Entity<StationRelation>().HasData(PrePopulateData.StationRelations);
+            modelBuilder.Entity<FlightHistory>().HasData(PrePopulateData.FlightHistories);
+            modelBuilder.Entity<ControlTower>().HasData(PrePopulateData.ControlTowers);
+            modelBuilder.Entity<StationControlTowerRelation>().HasData(PrePopulateData.StationControlTowerRelations);
+            return modelBuilder;
+        }
+    }
+}
